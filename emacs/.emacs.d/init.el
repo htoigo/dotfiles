0466b196668cef27002f510690d32a2347658ccc
Harry H. Toigo <hhtoigo@gmail.com>
2020-10-04 10:13:00 -0700

    Changed (quote (...)) form to '(...) in Customized variables in init.el.
    I think the latest version of Emacs (27.1?) did this automatically.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -11,12 +11,10 @@
  ;; If there is more than one, they won't work right.
  '(delete-selection-mode nil)
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
- '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t))))
+   '(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
+ '(org-babel-load-languages '((emacs-lisp . t) (shell . t)))
  '(package-selected-packages
-   (quote
-    (clojure-mode mu4e which-key use-package-chords try solarized-theme smex slime org-bullets oauth2 jump-char intero fill-column-indicator elpy better-defaults auto-compile auctex ample-theme ace-jump-mode 2048-game)))
+   '(clojure-mode mu4e which-key use-package-chords try solarized-theme smex slime org-bullets oauth2 jump-char intero fill-column-indicator elpy better-defaults auto-compile auctex ample-theme ace-jump-mode 2048-game))
  '(solarized-distinct-fringe-background t)
  '(solarized-use-more-italic t)
  '(x-underline-at-descent-line t))
