ad2d062ed8b827d5ab8852e87c862d6633a4a4ce
Harry H. Toigo II <hhtoigo@gmail.com>
2025-03-07 11:31:55 -0800

    Added new Emacs config hht-new to go back to vanilla Emacs from Spacemacs.
    I want to incorporate some of what I liked from Spacemacs, but regain control
    of my Emacs config. Spacemacs was hard to control.
    
    We have created a new config from my old hht config in order to experiment
    with things while preserving the old state of my config, so I can look back
    and see what I had done and compare with new settings.

---
+++ emacs/.emacs-configs/hht-new/init.el
@@ -0,0 +1,23 @@
+;;; Emacs initialization file
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+;;; Customization
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
+ '(custom-safe-themes
+   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(org-log-done 'time)
+ '(package-selected-packages
+   '(auctex slime nvm org-bullets ace-jump-mode company use-package-chords smex which-key spacemacs-theme try auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
