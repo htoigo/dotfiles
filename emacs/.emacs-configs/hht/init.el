082bd370b44d0ffffb163e9ab044cc013fdbed6a
Harry H. Toigo II <hhtoigo@gmail.com>
2025-03-07 11:01:45 -0800

    Some themes added to safe list in emacs hht/init.el file Customization section.
    This is in the emacs/.emacs-configs/hht/init.el version.
    
    '(custom-safe-themes
      '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" ...
        ... default))

--- emacs/.emacs-configs/hht/init.el
+++ emacs/.emacs-configs/hht/init.el
@@ -9,6 +9,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default))
  '(delete-selection-mode nil)
  '(elpy-modules
    '(elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
